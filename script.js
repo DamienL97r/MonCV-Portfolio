82dc20ee53b879970494729a7ca162629a180dd7
Damien Lebon <117284330+DamienL97r@users.noreply.github.com>
2022-12-05 11:06:41 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,94 @@
+
+
+//Popup morpion
+
+
+
+
+/*var popupActivator = document.getElementById('popupActivator');
+
+
+
+var popupMorpion = document.getElementById('popupMorpion');
+
+
+var textpopupMorpion = document.getElementById('textpopupMorpion');
+
+
+
+var btnClose = document.getElementById('btnClose');
+
+
+
+popupActivator.addEventListener('click',openModal);
+
+
+btnClose.addEventListener('click',closePopup);
+
+
+
+
+
+function openModal(){
+    popup2.style.display = 'flex';
+    textpopupAmZU.style.display = 'block';
+};
+
+
+function closePopup(){
+    popup2.style.display = 'none';
+};
+
+
+
+
+function openModal(){
+    popupMorpion.style.display = 'flex';
+    textpopupMorpion.style.display = 'block';
+};
+
+
+function closePopup(){
+    popupMorpion.style.display = 'none';
+};
+
+*/
+
+
+
+
+//Popup Among Z Us
+/*
+var popupActivator2 = document.getElementById('popupActivator2');
+
+
+
+var popup2 = document.getElementById('popup2');
+
+
+var textpopupAmZU = document.getElementById('textpopupAmZU');
+
+
+
+var btnClose2 = document.getElementById('btnClose2');
+
+
+popupActivator2.addEventListener('click',openModal);
+
+
+btnClose2.addEventListener('click',closePopup);
+
+function openModal(){
+    popup2.style.display = 'flex';
+    textpopupAmZU.style.display = 'block';
+};
+
+
+function closePopup(){
+    popup2.style.display = 'none';
+};
+
+*/
+
+
+
